4863eed1bd3e6180b97e2c09b8bf6caeca26bb54
Akimoto-Cris <akimotohisashibi@outlook.com>
2018-09-15 13:18:54 +0800

    Add files via upload

--- SLICcv.py
+++ SLICcv.py
@@ -213,7 +213,7 @@ img = cv2.imread(sys.argv[1])
 nr_superpixels = int(sys.argv[2])
 nc = int(sys.argv[3])
 
-step = int((img.shape[0]*img.shape[1] / nr_superpixels) ** 0.5)
+step = int((img.shape[0]*img.shape[1]/nr_superpixels)**0.5)
 
 slic = SLIC(img, step, nc)
 slic.generateSuperPixels()
